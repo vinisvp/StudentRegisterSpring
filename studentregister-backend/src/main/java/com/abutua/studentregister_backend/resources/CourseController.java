235e9e4e175e7ede9ac6eda101b2297feda299ed
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-07-28 16:06:34 -0300

    closes #3 - Made the GET function of the Course and Student Controller

--- studentregister-backend/src/main/java/com/abutua/studentregister_backend/resources/CourseController.java
+++ studentregister-backend/src/main/java/com/abutua/studentregister_backend/resources/CourseController.java
@@ -1,8 +1,31 @@
 package com.abutua.studentregister_backend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.abutua.studentregister_backend.models.Course;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
 public class CourseController {
-    
+    private List<Course> courses = new ArrayList<>();
+
+    @PostConstruct
+    private void init(){
+        Course c1 = new Course(1, "Java");
+        Course c2 = new Course(2, "Angular");
+        Course c3 = new Course(3, "HTML/CSS");
+        courses.add(c1);
+        courses.add(c2);
+        courses.add(c3);
+    }
+
+    @GetMapping("courses")
+    private List<Course> getCourses(){
+        return courses;
+    }
 }
--- studentregister-backend/src/main/java/com/abutua/studentregister_backend/resources/StudentController.java
+++ studentregister-backend/src/main/java/com/abutua/studentregister_backend/resources/StudentController.java
@@ -1,8 +1,47 @@
 package com.abutua.studentregister_backend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.abutua.studentregister_backend.models.Student;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
+@CrossOrigin
 public class StudentController {
-    
+
+    private List<Student> students = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        Student s1 = new Student(1, "Pedro Vinicius", "pedro.vinicius@abutua.com", "(11) 99999-9999", 1, 1);
+        Student s2 = new Student(2, "Glauco Todesco", "glauco.todesco@abutua.com", "(11) 99999-9999", 2, 2);
+        Student s3 = new Student(3, "John Doe", "john.doe@abutua.com", "(11) 99999-9999", 3, 3);
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("students/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable int id) {
+        Student student = students.stream()
+                          .filter(s -> s.getId() == id)
+                          .findFirst()
+                          .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found!"));
+        return ResponseEntity.ok(student);
+    }
 }
