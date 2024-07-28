c487f2f8c877ab78c656ed6a61320f1e63b1f4d3
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-07-28 16:30:54 -0300

    closes #4 - Added the POST function for the Student Controller

--- studentregister-backend/src/main/java/com/abutua/studentregister_backend/resources/StudentController.java
+++ studentregister-backend/src/main/java/com/abutua/studentregister_backend/resources/StudentController.java
@@ -1,5 +1,6 @@
 package com.abutua.studentregister_backend.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.studentregister_backend.models.Student;
 
@@ -25,12 +29,10 @@ public class StudentController {
     public void init() {
         Student s1 = new Student(1, "Pedro Vinicius", "pedro.vinicius@abutua.com", "(11) 99999-9999", 1, 1);
         Student s2 = new Student(2, "Glauco Todesco", "glauco.todesco@abutua.com", "(11) 99999-9999", 2, 2);
-        Student s3 = new Student(3, "John Doe", "john.doe@abutua.com", "(11) 99999-9999", 3, 3);
         students.add(s1);
         students.add(s2);
-        students.add(s3);
     }
-
+    
     @GetMapping("students")
     public List<Student> getStudents() {
         return students;
@@ -43,5 +45,18 @@ public class StudentController {
                           .findFirst()
                           .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found!"));
         return ResponseEntity.ok(student);
+    }
+
+    @PostMapping("students")
+    public ResponseEntity<Student> postStudent(@RequestBody Student student) {
+        student.setId(students.size() + 1);
+        students.add(student);
+        
+        URI location = ServletUriComponentsBuilder
+                       .fromCurrentRequest()
+                       .path("/{id}")
+                       .buildAndExpand(student.getId())
+                       .toUri();
+        return ResponseEntity.created(location).body(student);
     }
 }
